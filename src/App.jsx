e0493b882030aa555b445ab0977bfbe9d27d7f2d
add-ux <154344799+add-ux@users.noreply.github.com>
2026-04-16 22:30:32 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// App.jsx
+import './App.css';
+import LandingPage from './components/LandingPage';
+import Signup from "./components/signup";
+import Login from "./components/login";
+import AnalyzePage from "./components/analyzepage";
+import ErrorBoundary from './components/ErrorBoundary';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/analyze" element={<AnalyzePage />} />
+        </Routes>
+      </Router>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
